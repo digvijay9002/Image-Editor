a0acd42686a9f3efd8755acf53487461e26742a5
digvijay9002 <digvijay222002@gmail.com>
2024-03-07 17:58:44 +0530

    UI changes

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -34,79 +34,81 @@ const ImagePreview = () => {
 
   return (
     <div className="image-preview-container">
-      <div className="options-container">
-        <div className={`adjustment-options ${activeOption === editOptions.tune ? 'active' : ''}`}>
-          <span
-            className="material-symbols-outlined"
-            id="adjustment-options"
-            onClick={() => handleOptionClick(editOptions.tune)}
-          >
-            tune
-          </span>
+      <div className="options-wrapper">
+        <div className="options-container">
+          <div className={`adjustment-options ${activeOption === editOptions.tune ? 'active' : ''}`}>
+            <span
+              className="material-symbols-outlined"
+              id="adjustment-options"
+              onClick={() => handleOptionClick(editOptions.tune)}
+            >
+              tune
+            </span>
+          </div>
+          <div className={`filters-options ${activeOption === editOptions.filters ? 'active' : ''}`}>
+            <span
+              className="material-symbols-outlined"
+              onClick={() => handleOptionClick(editOptions.filters)}
+            >
+              style
+            </span>
+          </div>
+          <div className={`crop-option ${activeOption === editOptions.crop ? 'active' : ''}`}>
+            <span
+              className="material-symbols-outlined"
+              id="crop-box-btn"
+              onClick={() => {
+                handleOptionClick(editOptions.crop);
+                toggleCropBox();
+              }}
+            >
+              crop
+            </span>
+          </div>
+          <div className={`flip-horizontal-option ${activeOption === editOptions.flipHorizontal ? 'active' : ''}`}>
+            <span
+              className="material-symbols-outlined"
+              onClick={() => {
+                handleOptionClick(editOptions.flipHorizontal);
+                setFlipRotate((prev) => ({
+                  ...prev,
+                  flipHorizontal: prev.flipHorizontal === 1 ? -1 : 1,
+                }));
+              }}
+            >
+              flip
+            </span>
+          </div>
+          <div className={`flip-vertical-option ${activeOption === editOptions.flipVertical ? 'active' : ''}`}>
+            <span
+              className="material-symbols-outlined"
+              id="flipVertical"
+              onClick={() => {
+                handleOptionClick(editOptions.flipVertical);
+                setFlipRotate((prev) => ({
+                  ...prev,
+                  flipVertical: prev.flipVertical === 1 ? -1 : 1,
+                }));
+              }}
+            >
+              flip
+            </span>
+          </div>
+          <div className={`removeBackground ${activeOption === editOptions.removeBG ? 'active' : ''}`}>
+            <span
+              className="material-symbols-outlined"
+              onClick={() => {
+                handleOptionClick(editOptions.removeBG);
+                handleRemoveBG();
+              }}
+            >
+              background_replace
+            </span>
+          </div>
         </div>
-        <div className={`filters-options ${activeOption === editOptions.filters ? 'active' : ''}`}>
-          <span
-            className="material-symbols-outlined"
-            onClick={() => handleOptionClick(editOptions.filters)}
-          >
-            style
-          </span>
-        </div>
-        <div className={`crop-option ${activeOption === editOptions.crop ? 'active' : ''}`}>
-          <span
-            className="material-symbols-outlined"
-            id="crop-box-btn"
-            onClick={() => {
-              handleOptionClick(editOptions.crop);
-              toggleCropBox();
-            }}
-          >
-            crop
-          </span>
-        </div>
-        <div className={`flip-horizontal-option ${activeOption === editOptions.flipHorizontal ? 'active' : ''}`}>
-          <span
-            className="material-symbols-outlined"
-            onClick={() => {
-              handleOptionClick(editOptions.flipHorizontal);
-              setFlipRotate((prev) => ({
-                ...prev,
-                flipHorizontal: prev.flipHorizontal === 1 ? -1 : 1,
-              }));
-            }}
-          >
-            flip
-          </span>
-        </div>
-        <div className={`flip-vertical-option ${activeOption === editOptions.flipVertical ? 'active' : ''}`}>
-          <span
-            className="material-symbols-outlined"
-            id="flipVertical"
-            onClick={() => {
-              handleOptionClick(editOptions.flipVertical);
-              setFlipRotate((prev) => ({
-                ...prev,
-                flipVertical: prev.flipVertical === 1 ? -1 : 1,
-              }));
-            }}
-          >
-            flip
-          </span>
-        </div>
-        <div className={`removeBackground ${activeOption === editOptions.removeBG ? 'active' : ''}`}>
-          <span
-            className="material-symbols-outlined"
-            onClick={() => {
-              handleOptionClick(editOptions.removeBG);
-              handleRemoveBG();
-            }}
-          >
-            background_replace
-          </span>
-        </div>
-      </div>
-      <div className={`expanded-options`}>
-        {showActiveOptionsCompList[activeOption]}
+        {(activeOption === editOptions.tune || activeOption === editOptions.filters) && <div className={`expanded-options`}>
+          {showActiveOptionsCompList[activeOption]}
+        </div>}
       </div>
       <div className="image-canvas-btn-wrapper">
 
